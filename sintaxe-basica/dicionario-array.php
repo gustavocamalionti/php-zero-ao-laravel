1a3fe8926279a0df05de1c7c2e68a7f37f66476d
Gustavo Camalionti <54083715+gustavocamalionti@users.noreply.github.com>
2022-07-12 23:14:51 -0300

    Reorganizando o repositório
    Mudei o nome da pasta dos arquivos

---
+++ sintaxe-basica/dicionario-array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dicionário(associative) com Array</title>
+</head>
+<body>
+    <?php
+    $idade = array("Gustavo"=>"17", "Neri"=>"18", "Anderson"=>"19", "Giulia"=>"20");
+
+    echo "Anderson tem " . $idade['Anderson'] . " anos!";
+    ?>
+</body>
+</html>
