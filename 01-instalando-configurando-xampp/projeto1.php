3c547d289422892c8d0a4d24222005f253d1c582
Gustavo Camalionti <54083715+gustavocamalionti@users.noreply.github.com>
2022-07-12 21:49:45 -0300

    organizando o repositório | Primeiros commits

---
+++ 01-instalando-configurando-xampp/projeto1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula #2</title>
+</head>
+<body>
+    <h1>Gustavo Camalionti #2</h1>
+    <p>Para "compilar", devemos startar o xampp e acessar o seguinte link pelo navegador:</p>
+    <li>http://localhost/projects/php-zero-ao-laravel/01-instalando-configurando-xampp/projeto1.php</li>
+</body>
+</html>
---
+++ 02-variaveis-echo-concatenacao/variaveis.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $gustavo = 17;
+        $giulia = "A Giulia tem 22 anos";
+        echo $gustavo; //echo é um tipo de saída de dados. echo = envia os comandos para o servidor e ele retorna para página web
+        echo $giulia;
+    ?>
+</body>
+</html>
